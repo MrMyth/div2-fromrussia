ddcd441e728ac34f6dcd3f969c2e85f08de205e2
MrMyth <starchikov92@mail.ru>
2025-03-29 14:36:52 +0300

    Update resources.ts

--- src/constants/resources.ts
+++ src/constants/resources.ts
@@ -13,5 +13,8 @@ export const RESOURCES = [
   { name: "Twitch канал разработчиков", url: "https://www.twitch.tv/thedivisiongame", icon: Video },
   { name: "Официальный канал Ubisoft на Youtube", url: "https://www.youtube.com/@Ubisoft/featured", icon: Youtube },
   { name: "Реферальная программа The Division 2", url: "https://referral.ubisoft.com/the-division-2-referral/ru-RU", icon: Link },
-  { name: "Онлайн конструктор билдов (английский)", url: "https://mxswat.github.io/mx-division-builds/#/", icon: Link }
+  { name: "Онлайн конструктор билдов (английский)", url: "https://mxswat.github.io/mx-division-builds/#/", icon: Link },
+  { name: "Сообщество игры на платформе Reddit", url: "https://www.reddit.com/r/Division2/", icon: Link },
+  { name: "Discord сервер Ubisoft", url: "https://discord.gg/ubisoftofficial", icon: Link }
+  { name: "Discord сервер The Division 2", url: "https://discord.gg/thedivisiongame", icon: Link }
 ] as const;
