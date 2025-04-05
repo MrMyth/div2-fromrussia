ce9c9436b1c94981e60b89fa61ea7ddccf85beb2
MrMyth <starchikov92@mail.ru>
2025-04-05 13:16:34 +0300

    Update ComplaintForm.tsx

--- src/components/ComplaintForm.tsx
+++ src/components/ComplaintForm.tsx
@@ -1,19 +1,20 @@
-
 import React from 'react';
 
 const ComplaintForm = () => {
   return (
-    <div className="flex justify-center">
-      <iframe 
-        src="https://docs.google.com/forms/d/e/1FAIpQLSeyhEk7FC95iPaHFvHxNlRNIEh4D_qV7tQVM1Ic8DdpocXXBQ/viewform?embedded=true" 
-        width={640}
-        height={900}
-        frameBorder={0}
-        className="max-w-full"
-        title="Форма жалобы на игрока"
-      >
-        Загрузка…
-      </iframe>
+    <div className="flex justify-center px-4 pb-2">
+      <div className="w-full max-w-2xl border border-gray-200 rounded-lg overflow-hidden shadow-sm">
+        <iframe 
+          src="https://docs.google.com/forms/d/e/1FAIpQLSeyhEk7FC95iPaHFvHxNlRNIEh4D_qV7tQVM1Ic8DdpocXXBQ/viewform?embedded=true" 
+          width="100%"
+          height="650"
+          frameBorder="0"
+          className="block"
+          title="Форма жалобы на игрока"
+        >
+          Загрузка…
+        </iframe>
+      </div>
     </div>
   );
 };
