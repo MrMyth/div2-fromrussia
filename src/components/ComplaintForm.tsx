5f3da23e8432419d1522a509e515ad3c3eb92252
MrMyth <starchikov92@mail.ru>
2025-05-17 11:13:05 +0300

    Update ComplaintForm.tsx

--- src/components/ComplaintForm.tsx
+++ src/components/ComplaintForm.tsx
@@ -5,12 +5,18 @@ const ComplaintForm = () => {
     <div className="flex justify-center px-4 pb-2">
       <div className="w-full max-w-2xl border border-gray-200 rounded-lg overflow-hidden shadow-sm">
         <iframe 
-          src="https://docs.google.com/forms/d/e/1FAIpQLSeyhEk7FC95iPaHFvHxNlRNIEh4D_qV7tQVM1Ic8DdpocXXBQ/viewform?embedded=true" 
           width="100%"
           height="650"
+          src="https://forms.office.com/Pages/ResponsePage.aspx?id=DQSIkWdsW0yxEjajBLZtrQAAAAAAAAAAAAO__cxv-yNUNk9PQ1lEU1hWUDVFTUU1OEcwUVA0QVFZWi4u&embed=true" 
           frameBorder="0"
-          className="block"
+          marginWidth="0"
+          marginHeight="0"
+          className="border-none max-w-full max-h-screen"
           title="Форма жалобы на игрока"
+          allowFullScreen 
+          webkitallowfullscreen="true" 
+          mozallowfullscreen="true" 
+          msallowfullscreen="true"
         >
           Загрузка…
         </iframe>
