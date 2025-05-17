e79665c0ceef3e0173f566d656c22f268991d8b4
MrMyth <starchikov92@mail.ru>
2025-05-17 13:53:25 +0300

    Update Footer.tsx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -9,7 +9,7 @@ const Footer = () => {
             <p className="text-sm">Игровой Discord сервер</p>
           </div>
           <div className="text-sm text-right">
-            <p>Дата обновления сайта: 11:40 (МСК) 12.05.2025</p>
+            <p>Дата обновления сайта: 13:55 (МСК) 17.05.2025</p>
           </div>
         </div>
       </div>
