4f5a6966ae10207e3119f3eb941b9d302b1a1cd0
MrMyth <starchikov92@mail.ru>
2025-06-02 12:55:05 +0300

    Update menu.ts

--- src/constants/menu.ts
+++ src/constants/menu.ts
@@ -1,9 +1,10 @@
 
-import { MessageSquare, Youtube, BookOpen, Gamepad, HelpCircle, Link, Calendar, Globe } from "lucide-react";
+import { MessageSquare, Youtube, BookOpen, Gamepad, HelpCircle, Link, Calendar, Bot, Globe } from "lucide-react";
 
 export const MENU_ITEMS = [
   { icon: MessageSquare, text: "Discord сервер", href: "#discord" },
   { icon: Gamepad, text: "Наши игры", href: "#games" },
+  { icon: Bot, text: "Бот для Саммита", href: "#bot" },
   { icon: Globe, text: "Китайская версия", href: "#chinese-version" },
   { icon: BookOpen, text: "Азбуки рейдов", href: "#raids" },
   { icon: Youtube, text: "Наши каналы", href: "#channels" },
