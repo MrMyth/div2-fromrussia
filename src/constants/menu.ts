35e62d41e5e16133776147edf2c2d5d4589768a3
MrMyth <starchikov92@mail.ru>
2025-06-02 12:44:09 +0300

    Update menu.ts

--- src/constants/menu.ts
+++ src/constants/menu.ts
@@ -4,6 +4,7 @@ import { MessageSquare, Youtube, BookOpen, Gamepad, HelpCircle, Link, Calendar, 
 export const MENU_ITEMS = [
   { icon: MessageSquare, text: "Discord сервер", href: "#discord" },
   { icon: Gamepad, text: "Наши игры", href: "#games" },
+  { icon: Bot, text: "Бот для Саммита", href: "#bot" },
   { icon: Globe, text: "Китайская версия", href: "#chinese-version" },
   { icon: BookOpen, text: "Азбуки рейдов", href: "#raids" },
   { icon: Youtube, text: "Наши каналы", href: "#channels" },
